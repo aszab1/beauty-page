f4d4fd5bc978f87d5b58136eea18f0dc46ecc1bf
aszab1 <adry_szabo@yahoo.co.uk>
2024-10-22 17:10:00 +0100

    added motion animation to Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,25 @@
+import { useEffect } from 'react'
+import { motion } from 'framer-motion'
 import salon from '../assets/images/stock-photo1.webp'
 import dee from '../assets/images/dee.jpeg'
 
 export default function Home(){
 
+  
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  }
+
+
+
   return(
     <>
-    <img className='salon' src={salon} alt="pocture of salon" />
+    <img className='salon' src={salon} alt="picture of salon" />
     <section className='intro'>
       <div className='intro-text'>
-      <h3 className='welcome'>Welcome to</h3>
+      <motion.h3 className='welcome' variants={itemVariants}>Welcome to</motion.h3>
       <h4 className='beauty'>Enhance My Natural Beauty</h4>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate totam, nostrum cum eligendi cumque tenetur quod dignissimos, odio atque magni architecto vel laboriosam, laborum maxime ab culpa debitis veniam repellat?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos impedit quam blanditiis commodi at veniam sunt illo excepturi itaque, repudiandae alias accusantium accusamus mollitia sit ipsa dicta? Laborum, quis laudantium.</p>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { Nav, NavDropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './Nav'
 import { Icon } from '@iconify/react'
@@ -8,7 +10,8 @@ import { Icon } from '@iconify/react'
 
 export default function NavBar() {
 
-  const [showSkinBoosters, setShowSkinBoosters ] = useState(false)
+  const [showSkinBoosters, setShowSkinBoosters] = useState(false)
+  const location = useLocation()
 
   const toggleSkinBoosters = (e) => {
     e.stopPropagation() // Prevent dropdown from closing
@@ -30,45 +33,59 @@ export default function NavBar() {
     }
   }, [])
 
+  const navbarAnimation = {
+    hidden: { y: -100, opacity: 0 },
+    visible: { y: 0, opacity: 1, transition: { delay: 0.5, duration: 1 } },
+  }
+
 
   return (
     <>
-    
-    <Navbar className='navbar' fixed='top' expand='lg'>
+
+      <motion.Navbar
+        key={location.pathname}
+        className='navbar'
+        fixed='top'
+        expand='lg'
+        initial='hidden'
+        animate='visible'
+        variants={navbarAnimation}
+        style={{ position: 'absolute', top: 0, width: '100%' }}
+      >
         <Container className='d-flex flex-column' fluid>
-        
+
           <div className="nav-info">
-          <Nav className="w-100 justify-content-center">
-            <Nav.Link className="enhance-text" as={Link} to='/'>Enhance My Natural Beauty</Nav.Link>
-          </Nav>
-          <div>
-          <section className='email-div'>
-            <Icon className='email-icon' icon="ic:outline-email"></Icon>
-          <a className="email" href='mailto:info@email.com' >info@email.com</a>
-          </section>
-          <section className='mobile-div'>
-          <Icon className="phone-icon" icon="line-md:phone"></Icon>
-          <a className="mobile" href="tel:+447772519590">07772519590</a>
-          </section>
-          </div>
+            <Nav className="w-100 justify-content-center">
+              <Nav.Link className="enhance-text" as={Link} to='/'>Enhance My Natural Beauty</Nav.Link>
+            </Nav>
+            <div>
+              <section className='email-div'>
+                <Icon className='email-icon' icon="ic:outline-email"></Icon>
+                <a className="email" href='mailto:info@email.com' >info@email.com</a>
+              </section>
+              <section className='mobile-div'>
+                <Icon className="phone-icon" icon="line-md:phone"></Icon>
+                <a className="mobile" href="tel:+447772519590">07772519590</a>
+              </section>
+            </div>
           </div>
           <div className='divider'></div>
           <Nav className="nav-links w-100 justify-content-evenly">
             <Nav.Link as={Link} to='/'>HOME</Nav.Link>
-            
+
             <NavDropdown title={
-                <>
-                  TREATMENTS <Icon icon="mdi:chevron-down" /> 
-                </>
-              }
+              <>
+                TREATMENTS <Icon icon="mdi:chevron-down" />
+              </>
+            }
               id="treatments-dropdown"
             >
 
-        <NavDropdown.Item as={Link} to='/chemical-peels'>Chemical Peels</NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/microneedling'>Microneedling</NavDropdown.Item>
-        {/* <NavDropdown.Item as={Link} to='/skin-boosters'>Skin Boosters</NavDropdown.Item> */}
-        {/* Skin Boosters Accordion-Style Dropdown */}
-        <NavDropdown.Item as="div" onClick={toggleSkinBoosters} className="dropdown-accordion-toggle">
+              <NavDropdown.Item as={Link} to='/chemical-peels'>Chemical Peels</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to='/microneedling'>Microneedling</NavDropdown.Item>
+              {/* <NavDropdown.Item as={Link} to='/skin-boosters'>Skin Boosters</NavDropdown.Item> */}
+              {/* Skin Boosters Accordion-Style Dropdown */}
+              <NavDropdown.Item as="div" onClick={toggleSkinBoosters} className="dropdown-accordion-toggle">
                 Skin Boosters <Icon icon={showSkinBoosters ? "mdi:chevron-up" : "mdi:chevron-down"} /> {/* Toggle arrow */}
               </NavDropdown.Item>
               {showSkinBoosters && (
@@ -77,20 +94,20 @@ export default function NavBar() {
                   <NavDropdown.Item as={Link} to='/skin-boosters'>Eyes</NavDropdown.Item>
                 </div>
               )}
-        <NavDropdown.Item as={Link} to='/fat-dissolving'>Fat dissolving</NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/vitamin-shots'>Vitamin Shots</NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/jelly-masks'>Hyperboost Jelly Masks</NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/LED-light-therapy'>LED Light Therapy</NavDropdown.Item>
-        
-      </NavDropdown>
+              <NavDropdown.Item as={Link} to='/fat-dissolving'>Fat dissolving</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to='/vitamin-shots'>Vitamin Shots</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to='/jelly-masks'>Hyperboost Jelly Masks</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to='/LED-light-therapy'>LED Light Therapy</NavDropdown.Item>
+
+            </NavDropdown>
             <Nav.Link as={Link} to='/price-list'>PRICE LIST</Nav.Link>
             <Nav.Link as={Link} to='offers-packages'>OFFERS &amp; PACKAGES</Nav.Link>
             <Nav.Link as={Link} to='/contact'>CONTACT</Nav.Link>
-            
-      
+
+
           </Nav>
-          </Container>
-    </Navbar>   
+        </Container>
+      </motion.Navbar>
     </>
   )
 }
